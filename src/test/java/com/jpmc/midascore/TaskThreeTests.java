330f51379fb292ec7c191381d5e0c7c5c414f32e
Vedika Badhan <vedikaprash.badhan@gmail.com>
2026-06-18 17:01:31 +0530

    Update TaskThreeTests.java

--- src/test/java/com/jpmc/midascore/TaskThreeTests.java
+++ src/test/java/com/jpmc/midascore/TaskThreeTests.java
@@ -38,6 +38,8 @@ public class TaskThreeTests {
         logger.info("----------------------------------------------------------");
         logger.info("use your debugger to find out what waldorf's balance is after all transactions are processed");
         logger.info("kill this test once you find the answer");
+        UserRecord waldorf = userRepository.findByName("waldorf");
+System.out.println("Waldorf balance = " + waldorf.getBalance());
         while (true) {
             Thread.sleep(20000);
             logger.info("...");
